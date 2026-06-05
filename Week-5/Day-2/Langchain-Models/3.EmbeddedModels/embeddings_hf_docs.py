65b58bf93b2a7562eda39e92799f745c4e0a5f59
Chaudhari Muskan <muskan.chaudhari@aditiconsulting.com>
2026-06-05 17:41:36 +0530

    Week 5 Day 2 progress

---
+++ Week-5/Day-2/Langchain-Models/3.EmbeddedModels/embeddings_hf_docs.py
@@ -0,0 +1,16 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
+
+documents = [
+    "Delhi is the capital of India",
+    "Kolkata is the captital of West Bengal",
+    "Paris is the capital of France"
+]
+
+vector = embedding.embed_documents(documents)
+
+print(str(vector))
